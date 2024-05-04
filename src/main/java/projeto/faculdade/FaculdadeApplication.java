61524ff979b0c90dfd60d39caf51c87c6829c1d7
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-04 16:13:08 -0300

    first commit

---
+++ src/main/java/projeto/faculdade/FaculdadeApplication.java
@@ -0,0 +1,12 @@
+package projeto.faculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaculdadeApplication.class, args);
+	}
+}
